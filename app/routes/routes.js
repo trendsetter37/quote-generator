23c315ca7aa45f3a2ad1851e24939baea84cc6ee
Javis Sullivan <jsullivan@servosity.com>
2016-06-02 12:29:16 -0400

    allow x-Auth-token

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,7 +7,10 @@ var express = require('express'),
 
 router.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, x-Auth-token, X-Requested-With, Content-Type, Accept"
+	);
   next();
 });
 router.use('/authenticate', authUser);
