50835f3e5922518e33fc84668d5cc3eaa11b4cc0
Javis Sullivan <jsullivan@servosity.com>
2016-06-02 10:55:28 -0400

    add CORS header

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -5,6 +5,11 @@ var express = require('express'),
 	authMiddleware = require('./authMiddleware'),
 	authUser = require('./authUser');
 
+router.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 router.use('/authenticate', authUser);
 router.use(authMiddleware);
 
